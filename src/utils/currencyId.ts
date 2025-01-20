4208394931d31a5c819d651694af884a545521c5
alagunoff <lagunov.artamon@gmail.com>
2025-01-20 16:07:59 +0300

    feat: add new condition

--- src/utils/currencyId.ts
+++ src/utils/currencyId.ts
@@ -2,6 +2,7 @@ import { Currency } from '@alagunoff/uniswap-sdk-core'
 
 export function currencyId(currency: Currency): string {
   if (currency.isEther) return 'ETH'
+  if (currency.isPol) return 'POL'
   if (currency.isToken) return currency.address
   throw new Error('invalid currency')
 }
